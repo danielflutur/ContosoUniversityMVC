00f8a51c9509427ec9b122114f3150a948f02a87
Flutur Daniel-Ioan <ioan.flutur@student.usv.ro>
2022-07-10 17:55:36 +0300

    Added an about page

--- ContosoUniversityMVC/Controllers/HomeController.cs
+++ ContosoUniversityMVC/Controllers/HomeController.cs
@@ -1,21 +1,24 @@
-﻿using ContosoUniversityMVC.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.EntityFrameworkCore;
+using ContosoUniversityMVC.Data;
+using ContosoUniversityMVC.Models.SchoolViewModels;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ContosoUniversityMVC.Models;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace ContosoUniversityMVC.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly SchoolContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, SchoolContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -32,6 +35,18 @@ namespace ContosoUniversityMVC.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
         }
     }
 }
